88f8afb4a5b049f2fa3e200a1fbc37da3595509c
julie hawkins <julabi@gmail.com>
2017-10-23 13:52:17 -0600

    moves index and fixes packageJSON

---
+++ index.js
@@ -0,0 +1,13 @@
+const bubbleSort = require('./scripts/bubbleSort.js');
+const insertionSort = require('./scripts/insertionSort.js');
+const mergeSort = require('./scripts/mergeSort.js');
+const quickSort = require('./scripts/quickSort.js')
+
+module.exports = {
+  bubbleSort,
+  insertionSort,
+  mergeSort,
+  quickSort
+}
+
+
